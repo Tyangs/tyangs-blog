fe86af2a02f2d2d368b19529544a1299758cb542
tyangs <t.youngs0324@gmail.com>
2022-05-23 00:18:35 +0800

    :art: (lint-sort) lint sort import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import type { AppProps } from 'next/app';
-import {useState, useEffect} from 'react'
-import Layout from '@components/Layout';
-import { ThemeProvider } from '@context/ThemeProvider';
-
 import '../styles/globals.css';
 import '../styles/hljs.css';
+
+import Layout from '@components/Layout';
+import { ThemeProvider } from '@context/ThemeProvider';
+import type { AppProps } from 'next/app';
+import {useEffect,useState} from 'react'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	const [isSSR, setIsSSR] = useState<boolean>(true)
