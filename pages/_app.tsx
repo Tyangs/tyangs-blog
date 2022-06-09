c3a465707310d1d10a3f858f751812129097141c
tyangs <t.youngs0324@gmail.com>
2022-06-09 17:53:40 +0800

    :mag: (SEO-try) a SEO try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,17 @@ import '../styles/hljs.css';
 import Layout from '@components/Layout';
 import { ThemeProvider } from '@context/ThemeProvider';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 
 	return (
 		<ThemeProvider>
+			<Head>
+				<meta name="keywords" content="Tyangs Blog FrontEnd" />
+				<meta name="description" content="美丽新世界" />
+				<meta property='og:site_name' content="Tyangs's Blog" />
+			</Head>
 			{/* <Layout> */}
 				<Component {...pageProps} />
 			{/* </Layout> */}
